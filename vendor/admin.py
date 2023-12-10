70a97dfde81a4f93c73931937f238dd685377741
Den <dangluva@gmail.com>
2023-12-10 21:46:23 +0200

    vendor and customer dashboards edits

--- vendor/admin.py
+++ vendor/admin.py
@@ -2,8 +2,9 @@ from django.contrib import admin
 from vendor.models import Vendor
 
 class VendorAdmin(admin.ModelAdmin):
-    list_display = ('user', 'vendor_name', 'is_approved', 'created_at')
+    list_display = ('user', 'vendor_name', 'is_approved', 'created_at', 'modified_at')
     list_display_links = ('user', 'vendor_name')
+    list_editable = ('is_approved',)
 
 
 admin.site.register(Vendor, VendorAdmin)
