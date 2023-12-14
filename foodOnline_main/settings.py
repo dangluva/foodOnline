f2fb9d523f0315960651c89ce510e699f849fa60
DVR <135457531+dangluva@users.noreply.github.com>
2023-12-14 22:15:26 +0200

    Update settings.py (google api key hiding)

--- foodOnline_main/settings.py
+++ foodOnline_main/settings.py
@@ -148,4 +148,4 @@ EMAIL_HOST_PASSWORD = config('EMAIL_HOST_PASSWORD')
 EMAIL_USE_TLS = True
 DEFAULT_FROM_EMAIL = 'foodOnline Marketplace <myfoodonline@gmail.com>'
 
-GOOGLE_API_KEY = 'AIzaSyAPn-xau2CK9_736nO5Sqdd3gRHu9CPHTU'
+GOOGLE_API_KEY = config('GOOGLE_API_KEY')
